c0de4c693d07a0ecf0b2bc5771ed23f28f635daf
Kevin Viglucci <kviglucci@gmail.com>
2020-08-01 15:40:39 -0500

    reduce nesting with early return for error handling + add some comments

--- server.js
+++ server.js
@@ -28,36 +28,48 @@ app.get('/login', (req, res) => {
 
 app.get('/oauth/callback', async (req, res, next) => {
     const { code } = req.query;
+
+    // build headers
     const basicAuth = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
     const headers = {
         authorization: `Basic ${basicAuth}`,
         'Content-Type': 'application/x-www-form-urlencoded'
     };
+
+    // build request body
     const params = new URLSearchParams();
     params.append('redirect_uri', redirectUri);
     params.append('scope', scopes.join(' '));
     params.append('grant_type', 'authorization_code');
     params.append('code', code);
+
+    // execute request
     const requestOptions = {
         method: 'POST',
         body: params,
         headers
     };
     const oauthResponse = await fetch(TOKEN_ENDPOINT, requestOptions);
-    if (oauthResponse.ok) { // res.status >= 200 && res.status < 300
-        const responseData = await oauthResponse.json();
-        // do something with the `access_token` from `responseData`
-        // {
-        //     "access_token": "123456789",
-        //     "token_type": "bearer",
-        //     "expires_in": 86399,
-        //     "scope": "wow.profile"
-        // }
-        res.json(responseData);
-    } else {
+
+    // handle errors
+    if (!oauthResponse.ok) { // res.status >= 200 && res.status < 300
         console.log(`Token request failed with "${oauthResponse.statusText}"`);
-        next(new Error(oauthResponse.statusText));
+        return next(new Error(oauthResponse.statusText));
     }
+
+    // work with the oauth response
+    const responseData = await oauthResponse.json();
+
+    // do something with the `access_token` from `responseData`
+    // {
+    //     "access_token": "123456789",
+    //     "token_type": "bearer",
+    //     "expires_in": 86399,
+    //     "scope": "wow.profile"
+    // }
+
+    // send a JSON response (just an example)
+    res.json(responseData);
 });
 
 app.use((err, req, res, next) => {
